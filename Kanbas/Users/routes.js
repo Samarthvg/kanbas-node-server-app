87e7355a672ffe64012ee1bf5e22a99541e30e3e
Samarth <samarthgalchar@gmail.com>
2024-08-01 13:11:24 -0400

    sign up/in, profile routes and dao

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -1,5 +1,4 @@
 import * as dao from "./dao.js";
-let currentUser = null;
 export default function UserRoutes(app) {
     const createUser = async (req, res) => {
         const user = await dao.createUser(req.body);
@@ -17,21 +16,41 @@ export default function UserRoutes(app) {
       res.status(400).json(
         { message: "Username already taken" });
       return;
-    }
-    currentUser = await dao.createUser(req.body);
+    } 
+    const currentUser = await dao.createUser(req.body);
+    req.session["currentUser"] = currentUser;
     res.json(currentUser);
   };
 
   const signin = async (req, res) => {
     const { username, password } = req.body;
-    currentUser = await dao.findUserByCredentials(username, password);
-    res.json(currentUser);
-  };  
+    const currentUser = await dao.findUserByCredentials(username, password);
+    if (currentUser) {
+      req.session["currentUser"] = currentUser;
+      res.json(currentUser);
+    } else {
+      res.status(401).json({ message: "Unable to login. Try again later." });
+    }
+  };
 
-  const signout = (req, res) => { };
-  const profile = async (req, res) => {
+
+
+  const signout = (req, res) => {
+    req.session.destroy();
+
+    res.sendStatus(200);
+  };
+
+  
+  const profile = (req, res) => {
+    const currentUser = req.session["currentUser"];
+    if (!currentUser) {
+      res.sendStatus(401);
+      return;
+    }
     res.json(currentUser);
   };
+
 
   app.post("/api/users", createUser);
   app.put("/api/users/:userId", updateUser);
